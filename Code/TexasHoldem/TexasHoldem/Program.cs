d5c2158aa570c3397055b7a42e8478f15f2ff9bd
Nivlipetz1 <niv_lipetz@yahoo.com>
2017-04-10 01:22:12 +0300

    added project solution

---
+++ Code/TexasHoldem/TexasHoldem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("test");
+            Console.Read();
+
+        }
+    }
+}
