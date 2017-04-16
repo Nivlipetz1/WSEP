dc5aebb55bef315c7942760709c986e4d0e01180
ohaddali <ohadda@post.bgu.ac.il>
2017-04-17 01:21:32 +0300

    Added users interface

---
+++ Code/TexasHoldem/User/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    interface Users
+    {
+        bool register(string userName, string password);
+        bool login(string userName, string password);
+        bool logout();
+        bool editUserName(string userName);
+        bool editPassword(string password);
+    }
+}
