2ef397a71a45ec753fdfd96826742e01d71463b9
naordalal <naordal@post.bgu.ac.il>
2017-04-19 14:02:34 +0300

    add interface

---
+++ Code/TexasHoldem/TexasHoldem/GameCenterInterface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gaming;
+using User;
+
+namespace TexasHoldemSystem
+{
+    interface GameCenterInterface
+    {
+        bool createGame(GamePreferences preferecnces, UserProfile creatingUser, int buyIn);
+
+        List<Game> getAllSpectatingGames(UserProfile u);
+
+        List<Game> getAllActiveGamesByPlayerName(String playerName);
+
+        List<Game> getAllActiveGamesByPotSize(int potSize);
+
+        List<Game> getAllActiveGamesByGamePreference(GamePreferences preferences);
+
+        void joinGame(Game game, UserProfile u);
+
+        void spectateGame(Game game, UserProfile u);
+    }
+}
