e5caa5229382047fbde6fd387443eed1ed7d8355
naordalal <naordal@post.bgu.ac.il>
2017-04-24 22:42:47 +0300

    renamed namespaces

--- Code/TexasHoldem/TexasHoldem/System.cs
+++ Code/TexasHoldem/TexasHoldem/System.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
-using User;
+using GameUtilities;
 
 namespace TexasHoldemSystem
 {
--- Code/TexasHoldem/TexasHoldem/testUserSystem.cs
+++ Code/TexasHoldem/TexasHoldem/testUserSystem.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
-using User;
+using GameUtilities;
 
 namespace TexasHoldemSystem
 {
