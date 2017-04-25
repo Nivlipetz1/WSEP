22c55da18d9a8983bb5ae950230d618d2c4f55f3
Nivlipetz1 <niv_lipetz@yahoo.com>
2017-04-25 23:56:19 +0300

    fixed nunit

--- Code/TexasHoldem/TexasHoldem/testUserSystem.cs
+++ Code/TexasHoldem/TexasHoldem/testUserSystem.cs
@@ -1,11 +1,11 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using GameUtilities;
+using NUnit.Framework;
 
 namespace TexasHoldemSystem
 {
