fea9ff02630c0a30d58dcfd99bd90e797e001ce5
Omer Heller <hellero@post.bgu.ac.il>
2017-05-08 17:29:10 +0300

    first commit

---
+++ Code/GUI/GUI/GUI/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        MainWindow main;
+        public Login(MainWindow main)
+        {
+            this.main = main;
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (!username.Text.Equals(""))
+            {
+                main.statusFrame.NavigationService.Navigate(new Status(username.Text));
+                main.mainFrame.NavigationService.Navigate(new UserMainPage());
+            }
+            else
+            {
+                MessageBox.Show("Bad Input", "WARNING", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+    }
+}
