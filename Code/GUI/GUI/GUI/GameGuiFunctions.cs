1c9bdbdb5911b09b4428d6e695cba2246f6668ba
Omer Heller <hellero@post.bgu.ac.il>
2017-05-15 17:15:47 +0300

    merged GUI and Com.

---
+++ Code/GUI/GUI/GUI/GameGuiFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public class GameGuiFunctions
+    {
+        int gameID;
+        public GameGuiFunctions(int gameID)
+        {
+            this.gameID = gameID;
+        }
+
+    }
+}
