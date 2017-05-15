3e9b8f3a3de2e9b4d349579efa2458766be97968
Nivlipetz1 <niv_lipetz@yahoo.com>
2017-05-16 00:41:35 +0300

    edit avatar in edit profile page

--- Code/GUI/GUI/GUI/EditProfile.xaml.cs
+++ Code/GUI/GUI/GUI/EditProfile.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace GUI
 {
@@ -28,6 +29,20 @@ namespace GUI
         private void cancelBtn_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                //inserted correct avatar file
+            }
+
         }
     }
 }
