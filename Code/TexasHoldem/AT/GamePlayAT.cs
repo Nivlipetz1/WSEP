eff9ddd100d12e9c03dc8c2017fc823150ba7880
ohaddali <ohadda@post.bgu.ac.il>
2017-05-18 02:32:54 +0300

    Added null reference check to the gameService

--- Code/TexasHoldem/AT/GamePlayAT.cs
+++ Code/TexasHoldem/AT/GamePlayAT.cs
@@ -8,11 +8,35 @@ using ServiceLayer.Interfaces;
 using ServiceLayer;
 using ServiceLayer.Models;
 using AT.Stubs;
+using Gaming;
 
 namespace AT
 {
     [TestFixture]
     class GamePlayAT
-    { 
+    {
+        GameServiceInterface gs;
+        SystemService us;
+        GameCenterService gc;
+        string userName, Password;
+        int gameID;
+
+        [SetUp]
+        public void Before()
+        {
+            if (GameService.testable)
+                gs = new GameService();
+            else
+                gs = new GameServiceStub();
+            us = new SystemService();
+            gc = new GameCenterService();
+
+            userName = "Ohad";
+            Password = "1234";
+            us.register(userName, Password);
+            us.login(userName, Password);
+            GamePreferences pref = new GamePreferences(8, 2, 5, 10, 1, 2, 3, true);
+            gameID = gc.createGame(pref, userName).getID();
+        }
     }
 }
