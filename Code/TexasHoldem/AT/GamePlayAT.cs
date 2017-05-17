91b5d4f577fc45f35d7192f90a3bb773b7c3a8af
ohaddali <ohadda@post.bgu.ac.il>
2017-05-18 01:41:57 +0300

    Open GamePlayAT Class

---
+++ Code/TexasHoldem/AT/GamePlayAT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ServiceLayer.Interfaces;
+using ServiceLayer;
+using ServiceLayer.Models;
+using AT.Stubs;
+
+namespace AT
+{
+    [TestFixture]
+    class GamePlayAT
+    { 
+    }
+}
