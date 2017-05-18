1b5a338248206a650475b8b87c96c2cb8c1cd874
naordalal <naordal@post.bgu.ac.il>
2017-05-18 13:09:42 +0300

    fix models

--- Code/GUI/GUI/GUI/Models/ClientUserProfile.cs
+++ Code/GUI/GUI/GUI/Models/ClientUserProfile.cs
@@ -17,27 +17,31 @@ namespace GUI.Models
         public string Username
         {
             get { return username; }
+            set { username = value; }
         }
 
         public byte [] Avatar
         {
             get { return avatar; }
+            set { avatar = value; }
         }
 
         public int Credit
         {
             get { return credit; }
+            set { credit = value; }
         }
 
         public int LeagueId
         {
             get { return leagueId; }
-
+            set { leagueId = value; }
         }
 
         public Statistics UserStat
         {
             get { return userStat; }
+            set { userStat = value; }
         }
     }
 }
